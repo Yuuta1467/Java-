9d79573b2a79893524290efa0cab4abcf592bdf6
Yuuta1467 <s1341011we@gmail.com>
2023-06-27 11:56:59 +0000

    [Java出力テスト]

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello World !");
+        System.out.println(1);
+        System.out.println(100);
+        System.out.println(123.456);
+        System.out.println(true);
+        System.out.println("Hello " + "Java!");
+        System.out.println("Hello " + "Java " + "World!");
     }
 }
