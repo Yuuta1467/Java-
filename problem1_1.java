36e5287464ce03c0801c9662f3ca0158e62c0538
Yuuta1467 <s1341011we@gmail.com>
2023-06-28 09:55:31 +0000

    [Java学習記録2]

---
+++ problem1_1.java
@@ -0,0 +1,6 @@
+public class problem1_1 {
+
+	public static void main(String[] args) {
+		System.out.println("井上雄太");
+	}
+}
---
+++ problem1_4.java
@@ -0,0 +1,7 @@
+public class problem1_4 {
+
+	public static void main(String[] args) {
+		System.out.print(1 + " + " + 2 + " + " + 3 + " = " + (1+2+3));
+	}
+
+}
